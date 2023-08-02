b0e28c9e3d7179715539a9ff3699ec76d1dbb103
esteffanys <97310047+esteffanys@users.noreply.github.com>
2023-08-02 16:49:05 -0300

    Update filme.ts

--- src/app/home/filme.ts
+++ src/app/home/filme.ts
@@ -1,8 +1,16 @@
 export class Filme{
-    titulo!: string
-    diretor!: string
-    origem!: string
-    cartaz!: string
-    ano!: number
-  }
+    titulo: string ;
+    diretor: string;
+    origem: string;
+    cartaz: string;
+    ano: number;
+  
+Constructor(){
+ this.titulo=''
+    this.diretor=''
+    this.origem=''
+    this.cartaz=''
+    this.ano=0
+ }
+}
   
